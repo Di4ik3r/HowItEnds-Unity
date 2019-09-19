8cd531fb6c474ad4b676c4fad43fc9fb2e6d6dec
Tymofiev <ilia.tumofiev@gmail.com>
2019-09-19 20:53:33 +0300

    added editor script, empty object and attached mapgenerator to it

---
+++ Assets/Scripts/Editor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(MapGenerator))]
+public class TilePRNGMapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator map = target as MapGenerator;
+        if (DrawDefaultInspector())
+        {
+            map.GenerateMap();
+        }
+    }
+}
