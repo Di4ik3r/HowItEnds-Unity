9f9a23d7c2539eeb2f5c69eae4b41f948e4d8261
Tymofiev <ilia.tumofiev@gmail.com>
2020-02-23 17:47:47 +0200

    welcome day night system

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    private List<Creature> creatures;
+            
+    void Start()
+    {
+        UnityEditor.SceneView.FocusWindowIfItsOpen(typeof(UnityEditor.SceneView));
+        //Creature.digitalMap = this.map.digitalMap;
+        //Creature.objectMap = this.map.objectMap;
+
+        // creatures = CreateCreatures(10);
+        // creatures = CreateCreatures(5);
+        // creatures = CreateCreaturesInWater(5);
+
+        // creatures = new List<Creature>();
+        // creatures.Add(new Creature(new Vector2(0, 0), 0));
+        // creatures.Add(new Creature(new Vector2(0, 5), 0));
+        // creatures.Add(new Creature(new Vector2(5, 0), 0));
+
+        //new PathFinding();
+    }
+  
+    void Update() {
+
+    }
+
+    //private List<Creature> CreateCreatures(int amount)
+    //{
+    //    List<Creature> result = new List<Creature>();
+
+    //    for (int i = 0; i < amount; i++)
+    //    {
+    //        var random = Random.Range(0, groundCoordinates.Count - 1);
+    //        Vector3 pickedGroundCoordinates = groundCoordinates[random];
+    //        Vector2 position = new Vector2(pickedGroundCoordinates.x, pickedGroundCoordinates.z);
+    //        // Creature creature = new Creature(position, 0);
+    //        var creature = GroundCreature.Create(position, 0);
+    //        result.Add(creature);
+    //    }
+
+    //    return result;
+    //}
+
+    //private List<Creature> CreateCreaturesInWater(int amount) {
+    //    List<Creature> result = new List<Creature>();
+
+    //    for(int i = 0; i < amount; i++) {
+    //        var random = Random.Range(0, waterCoordinates.Count-1);
+    //        Vector3 pickedCoordinates = waterCoordinates[random];
+    //        Vector2 position = new Vector2(pickedCoordinates.x, pickedCoordinates.z);
+    //        // Creature creature = new Creature(position, 0);
+    //        var creature = WaterCreature.Create(position, 0);
+    //        result.Add(creature);
+    //    }
+
+    //    return result;
+    //}
+
+    //private void creatureCycle() {
+    //    foreach(Creature creature in creatures) {
+    //        creature.MakeMove();
+    //    }
+    //}
+
+    //public int[,] getDigitalMap(){ 
+    //    return this.map.digitalMap;
+    //}
+
+    //public GameObject[,] getObjectMap() {
+    //    return this.map.objectMap;
+    //}
+}
---
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -0,0 +1,80 @@
+﻿using Assets.Scripts.Map;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public Vector2 mapSize;   
+    public float noiseScale;
+    public int octaves;
+    [Range(0, 1)]
+    public float persistence;
+    [Range(0, 1)]
+    public float lacunarity;
+    public Vector2 offset;
+    public int seed;
+    [Range(-1.5f, 15)]
+    public float heightDifference;
+    [Range(0, 100)]
+    public float foodPercent;
+    [Range(0, 100)]
+    public float decorationPercent;
+    public GameObject cube;
+    public Material[] materials;
+    public GameObject[] decorations;
+
+    [HideInInspector]
+    public List<Vector3> groundCoordinates;
+    [HideInInspector]
+    public List<Vector3> waterCoordinates;
+    [HideInInspector]
+    public List<Vector3> foodCoordinates;
+    [HideInInspector]
+    public List<Vector3> decorationCoordinates;
+    [HideInInspector]
+    public Map map;
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    public void GenerateMap()
+    {
+        groundCoordinates = new List<Vector3>();
+        waterCoordinates = new List<Vector3>();
+        foodCoordinates = new List<Vector3>();
+        decorationCoordinates = new List<Vector3>();
+
+        string holderName = "Field";
+        if (transform.Find(holderName))
+        {
+            DestroyImmediate(transform.Find(holderName).gameObject);
+        }
+
+        Transform platform = new GameObject(holderName).transform;
+        platform.parent = transform;
+
+        holderName = "Decorations";
+        if (transform.Find(holderName))
+        {
+            DestroyImmediate(transform.Find(holderName).gameObject);
+        }
+
+        Transform decorationsPlatform = new GameObject(holderName).transform;
+        decorationsPlatform.parent = transform;
+
+        int width = System.Convert.ToInt32(mapSize.x);
+        int lenght = System.Convert.ToInt32(mapSize.y);
+
+        float[,] noiseArray = Noise.GenerateNoiseMap(width, lenght, seed, noiseScale, octaves, persistence, lacunarity, offset);
+
+        map = new Map(width, lenght);
+        map.CreateGameObjectMap(Instantiate, cube, platform, noiseArray, heightDifference);
+        map.PlaceFood((int)foodPercent);
+        map.PlaceDecoration((int)decorationPercent);
+        map.PaintMap(materials, noiseArray, heightDifference, groundCoordinates, waterCoordinates, decorationCoordinates, foodCoordinates);
+        map.LocateDecorations(Instantiate, decorations, decorationCoordinates, decorationsPlatform, cube);
+    }
+}
